f9a1364e208916106ae0d24762319aab2adc7bb6
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-07-15 10:33:56 +0100

    added City

---
+++ src/components/WeatherCard/City/index.tsx
@@ -0,0 +1,18 @@
+type Location = {
+  cityName: string;
+  country: string;
+  lon: string;
+  lat: string;
+};
+
+export default function City(location: Location) {
+  return (
+    <div className="city-box">
+      <h2>location.cityName</h2>
+      <p>location.country</p>
+      <div className="long-lat">
+        <p>location.lon</p> <p>location.lat</p>
+      </div>
+    </div>
+  );
+}
